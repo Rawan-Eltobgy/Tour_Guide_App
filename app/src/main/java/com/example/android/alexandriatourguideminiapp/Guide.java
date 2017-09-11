6c08f17aa3fa181606a58428d5a0254b574907b0
Rawan-Eltobgy <rawaneltobgy@gmail.com>
2017-09-11 17:23:32 +0200

    removed hard-coded strings

--- app/src/main/java/com/example/android/alexandriatourguideminiapp/Guide.java
+++ app/src/main/java/com/example/android/alexandriatourguideminiapp/Guide.java
@@ -4,17 +4,18 @@ package com.example.android.alexandriatourguideminiapp;
  * Created by user on 7/1/2017.
  */
 public class Guide {
-        private String mSite;
-        private String mAddress;
-        private int mImageResourceId = NO_IMAGE_PROVIDED;
-        private static final int NO_IMAGE_PROVIDED = -1;
+    private static final int NO_IMAGE_PROVIDED = -1;
+    private String mSite;
+    private String mAddress;
+    private int mImageResourceId = NO_IMAGE_PROVIDED;
 
-        public Guide(String site, String add ) {
-            mSite = site;
-            mAddress = add;
+    public Guide(String site, String add) {
+        mSite = site;
+        mAddress = add;
 
-        }
-    public Guide(String site, String add , int image ) {
+    }
+
+    public Guide(String site, String add, int image) {
         mSite = site;
         mAddress = add;
         mImageResourceId = image;
@@ -22,26 +23,24 @@ public class Guide {
     }
 
 
-
-
     public String getSite() {
-            return mSite;
+        return mSite;
 
-        }
-
-
-        public String getAddress() {
-            return mAddress;
-
-        }
-
-        public int getImageResourceId() {
-            return mImageResourceId;
-
-        }
-
-        public boolean hasImage() {
-            return mImageResourceId != NO_IMAGE_PROVIDED;
-        }
     }
+
+
+    public String getAddress() {
+        return mAddress;
+
+    }
+
+    public int getImageResourceId() {
+        return mImageResourceId;
+
+    }
+
+    public boolean hasImage() {
+        return mImageResourceId != NO_IMAGE_PROVIDED;
+    }
+}
 
--- app/src/main/java/com/example/android/alexandriatourguideminiapp/SimpleFragmentPagerAdapter.java
+++ app/src/main/java/com/example/android/alexandriatourguideminiapp/SimpleFragmentPagerAdapter.java
@@ -14,20 +14,24 @@ import android.support.v4.app.FragmentPagerAdapter;
  */
 public class SimpleFragmentPagerAdapter extends FragmentPagerAdapter {
     final int PAGE_COUNT = 4;
-    private String tabTitles[] = new String[] { "Beach", "Museum", "Restaurant" ,"Other" };
-    private Context context;
+    Context context;
+    private String[] tabTitles;
 
-    public SimpleFragmentPagerAdapter(FragmentManager fm, Context context) {
-        super(fm);
+    public SimpleFragmentPagerAdapter(FragmentManager fragmentManager, Context context) {
+        super(fragmentManager);
         this.context = context;
+        tabTitles = new String[]
+                {this.context.getResources().getString(R.string.beach),
+                        this.context.getResources().getString(R.string.museum),
+                        this.context.getResources().getString(R.string.restaurant),
+                        this.context.getResources().getString(R.string.other)};
     }
-
     @Override
 
     public Fragment getItem(int position) {
         if (position == 0) {
             return new BeachesFragment();
-        } else if (position == 1){
+        } else if (position == 1) {
             return new MuseumsFragment();
         } else {
             if (position == 2) {
@@ -43,6 +47,7 @@ public class SimpleFragmentPagerAdapter extends FragmentPagerAdapter {
         // Generate title based on item position
         return tabTitles[position];
     }
+
     @Override
     public int getCount() {
         return PAGE_COUNT;
